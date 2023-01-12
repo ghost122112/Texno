c58bff26ec17e83e5215d7f90029c51aa9a7fc14
Jaloliddin Zukhriddinov <jalol101.ghost@yandex.com>
2023-01-12 15:25:56 +0500

    .

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -37,3 +37,26 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "flex";  
   dots[slideIndex - 1].className += " active";
 }
+
+// list of services
+
+// script to show services
+
+var openCity = (evt, cityName) => {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("section-4__container__content-blocks__tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("section-4__container__content-tab__btn-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+// end of service script
