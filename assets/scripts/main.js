9ce3525cc8a56601584b84d7d3406e42232747f8
Jaloliddin Zukhriddinov <jalol101.ghost@yandex.com>
2023-01-17 13:39:33 +0500

    .

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -76,3 +76,20 @@ var closePopupForm = () => {
 
 // end of form popup script
 
+const data = { username: 'example' };
+
+fetch('/index.html', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
